e4e8be0fab605afefef602e5a6c608f7260a9d46
hermannainggolan <hermannainggolan08@gmail.com>
2023-03-24 11:11:22 +0700

    first commit

---
+++ herman.js
@@ -0,0 +1,6 @@
+const maju = document.querySelector('header ul');
+const tolu = document.querySelector(".menu");
+tolu.addEventListener('click', function(){
+   
+   maju.classList.toggle("slide");
+}); 
