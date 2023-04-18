d8009d72fb7e26a522d62ed87f21d397a5363a42
herman nainggolan <128620695+hermannainggolan@users.noreply.github.com>
2023-04-18 11:46:57 +0700

    Delete herman.js
    Signed-off-by: herman nainggolan <128620695+hermannainggolan@users.noreply.github.com>

--- herman.js
+++
@@ -1,9 +0,0 @@
-const maju = document.querySelector('header ul');
-const tolu = document.querySelector(".menu");
-tolu.addEventListener('click', function(){
-   
-   maju.classList.toggle("slide");
-}); 
-
-
-jkhgciuryjuhgi;ubhoih
